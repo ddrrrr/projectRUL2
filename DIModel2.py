09ed5d76c52661c3f5d8c285a4cda14e7dd93375
cyh.wne@gmail.com <cyh.wne@gmail.com>
2020-05-31 11:04:40 +0800

    20200531

--- DIModel2.py
+++ DIModel2.py
@@ -39,16 +39,24 @@ class FreEncoder(nn.Module):
         super(FreEncoder, self).__init__()
         self.fea_size = fea_size
         self.encoder = nn.Sequential(
-            nn.Linear(2560,512),
-            nn.Tanh(),
-            nn.Dropout(),
-            nn.Linear(512,fea_size),
+            nn.Conv1d(2560,512,1),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            # nn.Dropout(),
+            nn.Conv1d(512,128,1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            # nn.Dropout(),
+            nn.Conv1d(128,fea_size,1),
+            nn.BatchNorm1d(fea_size),
             nn.Tanh(),
         )
     def forward(self,x):
+        x = x.transpose(2,3)
+        x = x.contiguous().view(x.size(0),2560,-1)
+        x = self.encoder(x)
         x = x.transpose(1,2)
-        x = x.contiguous().view(x.size(0),-1,2560)
-        return self.encoder(x)
+        return x
 
 
 class CDC(nn.Module):
@@ -60,7 +68,7 @@ class CDC(nn.Module):
         self.seq_len = seq_len
         # self.encoder = TimeEncoder(fea_size)
         self.encoder = FreEncoder(fea_size)
-        self.rnn_hidden_size = 64
+        self.rnn_hidden_size = 256
         self.gru = nn.GRU(self.fea_size,self.rnn_hidden_size,1,bidirectional=False,batch_first=True)
         self.Wk = nn.ModuleList([nn.Linear(self.rnn_hidden_size,self.fea_size) for i in range(self.timestep)])
         self.softmax = nn.Softmax()
@@ -104,7 +112,7 @@ class CDC(nn.Module):
             linear = self.Wk[i]
             pred[i] = linear(c_t)
         for i in range(self.timestep):
-            total = torch.mm(encode_samples[i], torch.transpose(pred[i],0,1))
+            total = torch.mm(nn.functional.relu(encode_samples[i]), nn.functional.relu(torch.transpose(pred[i],0,1)))
             correct = torch.sum(torch.eq(torch.argmax(self.softmax(total),dim=0),torch.arange(0,batch)))
             nce += torch.sum(torch.diag(self.lsoftmax(total)))
         nce /= -1.*batch*self.timestep
@@ -112,33 +120,69 @@ class CDC(nn.Module):
 
         return accuracy, nce
 
+class Shuff(nn.Module):
+    def __init__(self,fea_size):
+        super(Shuff,self).__init__()
+        self.fea_size = fea_size
+        self.hidden_size = 256
+        self.gru = nn.GRU(fea_size,self.hidden_size,1,batch_first=True)
+        self.encoder = FreEncoder(fea_size)
+        self.linear = nn.Sequential(
+            nn.Linear(self.hidden_size,1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self,x):
+        z = self.encoder(x)
+        shuffle_z = torch.empty((z.size(0),z.size(1),z.size(2))).float().cuda()
+        for i in range(z.size(0)):
+            idx = torch.randperm(z.size(1))
+            shuffle_z[i] = z[i,idx,:].view(z.size(1),z.size(2))
+
+        cat_z = torch.cat([z,shuffle_z],dim=0)
+        cat_z = nn.functional.dropout(cat_z)
+        target = torch.cat([torch.ones(z.size(0),1).cuda(),torch.zeros(z.size(0),1).cuda()],dim=0)
+        idx = torch.randperm(cat_z.size(0))
+        cat_z = cat_z[idx]
+        target = target[idx]
+        out,_ = self.gru(cat_z,torch.zeros(1, cat_z.size(0), self.hidden_size).cuda())
+        out = out[:,-1,:].view(cat_z.size(0),self.hidden_size)
+        out = self.linear(out)
+
+        loss = nn.functional.binary_cross_entropy(out,target)
+        acc = torch.sum(torch.eq(torch.round(out),target)).float() / out.size(0)
+        return acc.item(),loss
+
 class Process():
     def __init__(self):
         self.dataset = DataSet.load_dataset(name = 'phm_data')
         self.lr = 1e-3
         self.optimizer = optim.Adam
-        self.epochs = 150
-        self.batches = 10
-        self.batch_size = 8
-        self.seq_len = 128
+        self.epochs = 300
+        self.batches = 20
+        self.batch_size = 128
+        self.seq_len = 32
         self.fea_size = 32
         self.train_bearings = ['Bearing1_1','Bearing1_2','Bearing2_1','Bearing2_2','Bearing3_1','Bearing3_2']
         self.test_bearings = ['Bearing1_3','Bearing1_4','Bearing1_5','Bearing1_6','Bearing1_7',
                                 'Bearing2_3','Bearing2_4','Bearing2_5','Bearing2_6','Bearing2_7',
                                 'Bearing3_3']
 
-        self.network = CDC(32,self.batch_size,self.seq_len,self.fea_size).cuda()
+        # self.network = CDC(16,self.batch_size,self.seq_len,self.fea_ size).cuda()
+        self.network = Shuff(self.fea_size).cuda()
 
     def Begin(self):
         train_iter = self._preprocess('train')
         test_iter = self._preprocess('test')
-        optimizer = self.optimizer(self.network.parameters(),lr=self.lr)
+        optimizer = self.optimizer(self.network.parameters(),lr=self.lr,
+            betas=(0.9, 0.98), eps=1e-09, weight_decay=1e-4, amsgrad=True)
+        train_idx = [2750,830,870,750,490,1450]
 
         for e in range(1, self.epochs+1):
             train_loss = 0
             train_acc = 0
             for _ in range(self.batches):
-                temp_acc, temp_loss = self._cal_one_batch(train_iter, isTrain=True, opti=optimizer)
+                temp_acc, temp_loss = self._cal_one_batch(train_iter, isTrain=True, opti=optimizer,train_idx=train_idx)
                 train_acc += temp_acc
                 train_loss += temp_loss
             test_acc, test_loss = self._cal_one_batch(test_iter)
@@ -147,6 +191,11 @@ class Process():
                 % (e, train_loss/self.batches, train_acc/self.batches, test_loss, test_acc))
 
             torch.save(self.network.encoder,datetime.datetime.now().strftime("%Y%m%d") + 'encoder.pkl')
+
+            if (e>10 and e%5==0):
+                for i in range(len(train_idx)):
+                    if train_idx[i] >=0:
+                        train_idx[i] = max(0,train_idx[i] - random.randint(0,round(train_iter[i].shape[0]*0.03)))
 
     def _preprocess(self, select):
         if select == 'train':
@@ -175,8 +224,8 @@ class Process():
                 / np.repeat((np.max(fft_data,axis=2,keepdims=True) - np.min(fft_data,axis=2,keepdims=True)),data_len,axis=2)
         return fft_data
 
-    def _cal_one_batch(self, data_iter, isTrain=False, opti=None):
-        train_idx = [1299,688,459,179,300,1200]
+    def _cal_one_batch(self, data_iter, isTrain=False, opti=None,train_idx=None):
+        
         if isTrain:
             self.network.train()
         else:
@@ -189,11 +238,17 @@ class Process():
         data_len = data_iter[0].shape[-1]
         for i,x in enumerate(each_data_num):
             if isTrain:
-                temp_idx = np.random.randint(train_idx[i],data_iter[i].shape[0]-self.seq_len,size=x)
+                temp_idx = np.random.randint(max(self.seq_len,train_idx[i]),data_iter[i].shape[0],size=x)
             else:
-                temp_idx = np.random.randint(0,data_iter[i].shape[0]-self.seq_len,size=x)
+                temp_idx = np.random.randint(self.seq_len,data_iter[i].shape[0],size=x)
+
             for one_idx in temp_idx:
-                batch_data.append(data_iter[i][one_idx:one_idx+self.seq_len,:,:].reshape([1,2,self.seq_len,data_len]))
+                if isTrain:
+                    temp_jiange = np.random.randint(1,one_idx//self.seq_len+1)
+                else:
+                    temp_jiange=1
+                batch_data.append(data_iter[i][one_idx-self.seq_len*temp_jiange:one_idx:temp_jiange,:,:].reshape([1,2,self.seq_len,data_len]))
+
         batch_data = np.concatenate(batch_data, axis=0)
         batch_data = Variable(torch.from_numpy(batch_data.copy()).type(torch.FloatTensor)).cuda()
         acc,loss = self.network(batch_data)
@@ -217,11 +272,11 @@ class Process():
         for i in range(len(bearing_data)):
             temp_one_data = bearing_data[i]
             temp_one_rul = np.arange(temp_one_data.shape[0])[::-1] + bearing_rul[i]
-            pe = (np.exp(temp_one_rul+1)).reshape([-1,1])
+            pe = (np.log(temp_one_rul+1)).reshape([-1,1])
             temp_one_data = Variable(torch.from_numpy(temp_one_data.copy()).type(torch.FloatTensor)).cuda() 
             temp_one_feature_list = []
             for j in range(temp_one_data.shape[0]//64+1):
-                temp_one_fea = feature_net(temp_one_data[j*64:min(j*64+64,temp_one_data.shape[0]-1)])
+                temp_one_fea = feature_net(temp_one_data[j*64:min(j*64+64,temp_one_data.shape[0]-1)].unsqueeze(1))
                 temp_one_feature_list.append(temp_one_fea.data.cpu().numpy().reshape([temp_one_fea.shape[0],-1]))
             temp_one_fea = np.concatenate(temp_one_feature_list,axis=0)
 
